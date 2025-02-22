81263b0880d3f644757cab75032109ea05f24427
Batki Domonkos <domonkosbatki98@gmail.com>
2025-02-22 22:37:52 +0100

    Added RegisterView, LoginView and Progressring

---
+++ ShoppingList/ShoppingList/Views/RegisterView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ShoppingList;
+
+public partial class RegisterView : UserControl
+{
+    public RegisterView()
+    {
+        InitializeComponent();
+    }
+}
