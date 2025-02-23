c63088c7425fa86ef7cd569618d7b4be0b1ae813
Batki Domonkos <domonkosbatki98@gmail.com>
2025-02-23 22:47:52 +0100

    Client side login validation

---
+++ ShoppingList/ShoppingList/Utils/StringResolver.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using System.Collections.Generic;
+
+namespace ShoppingList.Utils
+{
+    public static class StringResolver
+    {
+        public static string GetString(string key)
+        {
+            Application.Current!.TryFindResource(key, out var resource);
+            return resource as string ?? throw new KeyNotFoundException();
+        }
+    }
+}
--- ShoppingList/ShoppingList/ViewModels/LoginViewModel.cs
+++ ShoppingList/ShoppingList/ViewModels/LoginViewModel.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ReactiveUI;
 using ShoppingList.Persistor;
-using ShoppingList.Persistor.Services;
 using ShoppingList.Persistor.Services.Interfaces;
+using ShoppingList.Utils;
 using System;
 using System.Reactive;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ShoppingList.ViewModels
@@ -16,6 +15,13 @@ namespace ShoppingList.ViewModels
         public string EmailInput { get; set; } = string.Empty;
         public string PasswordInput { get; set; } = string.Empty;
         public ReactiveCommand<Unit, Task> LoginCommand { get; }
+
+        private string? _errorMessage;
+        public string? ErrorMessage
+        {
+            get { return _errorMessage; }
+            set { this.RaiseAndSetIfChanged(ref _errorMessage, value); }
+        }
 
         private Action<bool> ShowLoading { get; }
         private Action<Page> ChangePage { get; }
@@ -30,6 +36,8 @@ namespace ShoppingList.ViewModels
 
         private async Task Login()
         {
+            if (!Validate()) return;
+
             ShowLoading(true);
 
             IUserService userService = AppServiceProvider.Services.GetRequiredService<IUserService>();
@@ -37,6 +45,59 @@ namespace ShoppingList.ViewModels
 
             ShowLoading(false);
             ChangePage(Page.Main);
+        }
+        private bool Validate()
+        {
+            if(ValidateEmail() && ValidatePassword())
+            {
+                ErrorMessage = null;
+                return true;
+            }
+            return false;
+        }
+        private bool ValidateEmail()
+        {
+            var trimmedEmail = EmailInput.Trim();
+
+            if (string.IsNullOrWhiteSpace(trimmedEmail))
+            {
+                ErrorMessage = StringResolver.GetString("EmailMissingError");
+                return false;
+            }
+
+            if (trimmedEmail.EndsWith('.'))
+            {
+                ErrorMessage = StringResolver.GetString("EmailFormatError");
+                return false;
+            }
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(EmailInput);
+                return addr.Address == trimmedEmail;
+            }
+            catch
+            {
+                ErrorMessage = StringResolver.GetString("EmailFormatError");
+                return false;
+            }
+        }
+        private bool ValidatePassword()
+        {
+            string trimmedPassword = PasswordInput.Trim();
+
+            if (string.IsNullOrWhiteSpace(trimmedPassword))
+            {
+                ErrorMessage = StringResolver.GetString("PasswordMissingError");
+                return false;
+            }
+
+            if(trimmedPassword.Length < 8)
+            {
+                ErrorMessage = StringResolver.GetString("PasswordFormatError");
+                return false;
+            }
+
+            return true;
         }
     }
 }
