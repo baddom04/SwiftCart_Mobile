32aac010ef6c92ed4b82cb82970c3466c6956bd6
Batki Domonkos <domonkosbatki98@gmail.com>
2025-02-27 14:28:08 +0100

    settingspage changes

--- ShoppingList/ShoppingList/ViewModels/SettingsViewModel.cs
+++ ShoppingList/ShoppingList/ViewModels/SettingsViewModel.cs
@@ -5,45 +5,68 @@ using System;
 using System.Reactive;
 using System.Threading.Tasks;
 
-namespace ShoppingList.ViewModels
+namespace ShoppingList.ViewModels;
+
+internal class SettingsViewModel : ViewModelBase
 {
-    internal class SettingsViewModel : ViewModelBase
+    private readonly UserAccountModel _userAccount;
+    public ReactiveCommand<Unit, Unit> LogoutCommand { get; }
+    public ReactiveCommand<Unit, Unit> DeleteUserCommand { get; }
+
+    private readonly Action<bool> _showLoading;
+    private readonly Action<Page> _changePage;
+    private readonly Action<NotificationType, string> _showNotification;
+
+    public SettingsViewModel(UserAccountModel userAccount, Action<Page> changePage, Action<bool> showLoading, Action<NotificationType, string> showNotification)
     {
-        private readonly UserAccountModel _userAccount;
-        public ReactiveCommand<Unit, Unit> LogoutCommand { get; }
+        _userAccount = userAccount;
+        _changePage = changePage;
+        _showLoading = showLoading;
+        _showNotification = showNotification;
 
-        private readonly Action<bool> _showLoading;
-        private readonly Action<Page> _changePage;
-        private readonly Action<NotificationType, string> _showNotification;
+        LogoutCommand = ReactiveCommand.CreateFromTask(Logout);
+        DeleteUserCommand = ReactiveCommand.CreateFromTask(DeleteUser);
+    }
 
-        public SettingsViewModel(UserAccountModel userAccount, Action<Page> changePage, Action<bool> showLoading, Action<NotificationType, string> showNotification)
+    private async Task DeleteUser()
+    {
+        _showLoading(true);
+
+        try
         {
-            _userAccount = userAccount;
-            _changePage = changePage;
-            _showLoading = showLoading;
-            _showNotification = showNotification;
-            LogoutCommand = ReactiveCommand.CreateFromTask(Logout);
+            await _userAccount.DeleteUserAsync();
+
+            _changePage(Page.Login);
         }
-
-        private async Task Logout()
+        catch (Exception ex)
         {
-            _showLoading(true);
+            string message = $"{StringProvider.GetString("DeleteUserError")}{ex.Message}";
+            _showNotification(NotificationType.Error, message);
+        }
+        finally
+        {
+            _showLoading(false);
+        }
+    }
 
-            try
-            {
-                await _userAccount.LogoutAsync();
+    private async Task Logout()
+    {
+        _showLoading(true);
 
-                _changePage(Page.Login);
-            }
-            catch (Exception ex)
-            {
-                string message = $"{StringProvider.GetString("LogoutError")}{ex.Message}";
-                _showNotification(NotificationType.Error, message);
-            }
-            finally
-            {
-                _showLoading(false);
-            }
+        try
+        {
+            await _userAccount.LogoutAsync();
+
+            _changePage(Page.Login);
+        }
+        catch (Exception ex)
+        {
+            string message = $"{StringProvider.GetString("LogoutError")}{ex.Message}";
+            _showNotification(NotificationType.Error, message);
+        }
+        finally
+        {
+            _showLoading(false);
         }
     }
 }
