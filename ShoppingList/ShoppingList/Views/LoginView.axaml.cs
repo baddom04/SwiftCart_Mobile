ba07957f18b3b0073698faeebc958fc3431e93eb
Batki Domonkos <domonkosbatki98@gmail.com>
2025-02-27 12:21:01 +0100

    Android fix

--- ShoppingList/ShoppingList/Views/LoginView.axaml.cs
+++ ShoppingList/ShoppingList/Views/LoginView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using ShoppingList.ViewModels;
 
 namespace ShoppingList.Views;
 
@@ -7,5 +8,14 @@ public partial class LoginView : UserControl
     public LoginView()
     {
         InitializeComponent();
+
+        Loaded += LoginView_Loaded;
+    }
+
+    private void LoginView_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        LoginViewModel viewModel = (DataContext as LoginViewModel)!;
+
+
     }
 }
