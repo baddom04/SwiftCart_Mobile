8481d51ee525d880851bb95a89c0f455f12c0820
Batki Domonkos <domonkosbatki98@gmail.com>
2025-02-28 16:00:57 +0100

    Household service

---
+++ ShoppingList.Persistor/Services/HouseholdService.cs
@@ -0,0 +1,86 @@
+﻿using ShoppingList.Core;
+using ShoppingList.Persistor.Services.Interfaces;
+using System.Net.Http.Json;
+
+namespace ShoppingList.Persistor.Services
+{
+    public class HouseholdService(HttpClient client) : APIService(client), IHouseholdService
+    {
+        public async Task CreateNewHouseholdAsync(string name, string identifier, CancellationToken cancellationToken = default)
+        {
+            var payload = new { name, identifier };
+
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("households", payload, cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+        }
+
+        public async Task DeleteHouseholdAsync(int household_id, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await _httpClient.DeleteAsync($"households/{household_id}", cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+        }
+
+        public async Task<IEnumerable<Household>> GetAllHouseholdsAsync(CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("households", cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+
+            IEnumerable<Household>? households = await response.Content.ReadFromJsonAsync<IEnumerable<Household>>(cancellationToken);
+            if (households == null)
+                throw new NullReferenceException(nameof(households));
+
+            return households;
+        }
+
+        public async Task<Household> GetHouseholdByIdAsync(int household_id, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"households/{household_id}", cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+
+            Household? household = await response.Content.ReadFromJsonAsync<Household>(cancellationToken);
+            if (household == null)
+                throw new NullReferenceException(nameof(household));
+
+            return household;
+        }
+
+        public async Task<IEnumerable<Household>> GetOwnedHouseholdsAsync(int user_id, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"users/{user_id}/households", cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+
+            IEnumerable<Household>? households = await response.Content.ReadFromJsonAsync<IEnumerable<Household>>(cancellationToken);
+            if (households == null)
+                throw new NullReferenceException(nameof(households));
+
+            return households;
+        }
+
+        public async Task<IEnumerable<User>> GetUsersOfHouseholdAsync(int household_id, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"households/{household_id}/users", cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+
+            IEnumerable<User>? households = await response.Content.ReadFromJsonAsync<IEnumerable<User>>(cancellationToken);
+            if (households == null)
+                throw new NullReferenceException(nameof(households));
+
+            return households;
+        }
+
+        public async Task UpdateHouseholdAsync(int household_id, string? name, string? identifier, CancellationToken cancellationToken = default)
+        {
+            var payload = new { name, identifier };
+
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync($"households/{household_id}", payload, cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+        }
+    }
+}
---
+++ ShoppingList.Persistor/Services/Interfaces/IHouseholdService.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingList.Core;
+
+namespace ShoppingList.Persistor.Services.Interfaces;
+
+public interface IHouseholdService
+{
+    Task CreateNewHouseholdAsync(string name, string identifier, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Household>> GetAllHouseholdsAsync(CancellationToken cancellationToken = default);
+    Task<IEnumerable<User>> GetUsersOfHouseholdAsync(int household_id, CancellationToken cancellationToken = default);
+    Task<Household> GetHouseholdByIdAsync(int household_id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Household>> GetOwnedHouseholdsAsync(int user_id, CancellationToken cancellationToken = default);
+    Task DeleteHouseholdAsync(int household_id, CancellationToken cancellationToken = default);
+    Task UpdateHouseholdAsync(int household_id, string? name, string? identifier, CancellationToken cancellationToken = default);
+}
