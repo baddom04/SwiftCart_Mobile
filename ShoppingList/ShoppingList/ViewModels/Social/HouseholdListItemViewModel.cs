9e0db83a729f5a97dd8fb1316fce023f7059882b
Batki Domonkos <domonkosbatki98@gmail.com>
2025-03-03 17:11:38 +0100

    Household applying viewmodel

---
+++ ShoppingList/ShoppingList/ViewModels/Social/HouseholdListItemViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ReactiveUI;
+using ShoppingList.Core;
+using System.Reactive;
+using System.Threading.Tasks;
+
+namespace ShoppingList.ViewModels.Social
+{
+    internal class HouseholdListItemViewModel : ViewModelBase
+    {
+        public Household Household { get; private set; }
+        public ReactiveCommand<Unit, Unit> ApplyCommand { get; }
+
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            set { this.RaiseAndSetIfChanged(ref _isLoading, value); }
+        }
+
+        public HouseholdListItemViewModel(Household household)
+        {
+            Household = household;
+            ApplyCommand = ReactiveCommand.CreateFromTask(Apply);
+        }
+
+        private async Task Apply()
+        {
+            IsLoading = true;
+            await Task.Delay(5000);
+            IsLoading = false;
+        }
+    }
+}
--- ShoppingList/ShoppingList/ViewModels/Social/SocialPanelViewModel.cs
+++ ShoppingList/ShoppingList/ViewModels/Social/SocialPanelViewModel.cs
@@ -1,88 +1,91 @@
 ﻿using DynamicData;
 using ReactiveUI;
-using ShoppingList.Core;
 using ShoppingList.Model.Models;
 using ShoppingList.Utils;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive;
 using System.Threading.Tasks;
 
-namespace ShoppingList.ViewModels.Social
+namespace ShoppingList.ViewModels.Social;
+
+internal class SocialPanelViewModel : ViewModelBase
 {
-    internal class SocialPanelViewModel : ViewModelBase
+    public string SearchInput { get; set; } = string.Empty;
+    public ReactiveCommand<Unit, Unit> SearchCommand { get; }
+    public ReactiveCommand<Unit, Unit> TurnPageForwardCommand { get; }
+    public ReactiveCommand<Unit, Unit> TurnPageBackwardCommand { get; }
+    public ObservableCollection<HouseholdListItemViewModel> Households { get; } = [];
+
+    private bool _showLoading;
+    public bool IsLoading
     {
-        public string SearchInput { get; set; } = string.Empty;
-        public ReactiveCommand<Unit, Unit> SearchCommand { get; }
-        public ReactiveCommand<Unit, Unit> TurnPageForwardCommand { get; }
-        public ReactiveCommand<Unit, Unit> TurnPageBackwardCommand { get; }
-        public ObservableCollection<Household> Households { get; } = [];
+        get { return _showLoading; }
+        set { this.RaiseAndSetIfChanged(ref _showLoading, value); }
+    }
 
-        private bool _showLoading;
-        public bool IsLoading
+    private int _page;
+    public int Page
+    {
+        get { return _page; }
+        set { this.RaiseAndSetIfChanged(ref _page, value); }
+    }
+
+    private int _maxPage;
+    public int MaxPage
+    {
+        get { return _maxPage; }
+        private set { this.RaiseAndSetIfChanged(ref _maxPage, value); }
+    }
+
+
+    public bool EmptyHouseholds => Households.Count == 0;
+
+    private readonly MainSocialPanelModel _model;
+    private readonly Action<NotificationType, string> _showNotification;
+
+    public SocialPanelViewModel(MainSocialPanelModel householdsModel, Action<NotificationType, string> showNotification)
+    {
+        _model = householdsModel;
+        _showNotification = showNotification;
+
+        Households.CollectionChanged += (s, e) => this.RaisePropertyChanged(nameof(EmptyHouseholds));
+
+        SearchCommand = ReactiveCommand.CreateFromTask(() => Search(1));
+
+        TurnPageForwardCommand = ReactiveCommand.CreateFromTask(() => Search(Page + 1), 
+            this.WhenAnyValue(x => x.Page, x => x.MaxPage,
+        (page, maxPage) => page != maxPage));
+
+        TurnPageBackwardCommand = ReactiveCommand.CreateFromTask(() => Search(Page - 1), 
+            this.WhenAnyValue(x => x.Page, page => page != 1));
+    }
+
+    public async Task Search(int page = 1)
+    {
+        IsLoading = true;
+        Page = page;
+
+        try
         {
-            get { return _showLoading; }
-            set { this.RaiseAndSetIfChanged(ref _showLoading, value); }
+            Households.Clear();
+            Households.AddRange(
+                (await _model.SearchHouseholdsAsync(SearchInput, Page))
+                .Select(hh => new HouseholdListItemViewModel(hh))
+            );
+
+            MaxPage = _model.MaxPage;
         }
-
-        private int _page;
-        public int Page
+        catch(Exception ex)
         {
-            get { return _page; }
-            set { this.RaiseAndSetIfChanged(ref _page, value); }
+            string message = $"{StringProvider.GetString("HouseholdQueryError")}{ex.Message}";
+            _showNotification(NotificationType.Error, message);
         }
-
-        private int _maxPage;
-        public int MaxPage
+        finally
         {
-            get { return _maxPage; }
-            private set { this.RaiseAndSetIfChanged(ref _maxPage, value); }
-        }
-
-
-        public bool EmptyHouseholds => Households.Count == 0;
-
-        private readonly MainSocialPanelModel _model;
-        private readonly Action<NotificationType, string> _showNotification;
-
-        public SocialPanelViewModel(MainSocialPanelModel householdsModel, Action<NotificationType, string> showNotification)
-        {
-            _model = householdsModel;
-            _showNotification = showNotification;
-
-            Households.CollectionChanged += (s, e) => this.RaisePropertyChanged(nameof(EmptyHouseholds));
-
-            SearchCommand = ReactiveCommand.CreateFromTask(() => Search(1));
-
-            TurnPageForwardCommand = ReactiveCommand.CreateFromTask(() => Search(Page + 1), 
-                this.WhenAnyValue(x => x.Page, x => x.MaxPage,
-            (page, maxPage) => page != maxPage));
-
-            TurnPageBackwardCommand = ReactiveCommand.CreateFromTask(() => Search(Page - 1), 
-                this.WhenAnyValue(x => x.Page, page => page != 1));
-        }
-
-        public async Task Search(int page = 1)
-        {
-            IsLoading = true;
-            Page = page;
-
-            try
-            {
-                Households.Clear();
-                Households.AddRange(await _model.SearchHouseholdsAsync(SearchInput, Page));
-
-                MaxPage = _model.MaxPage;
-            }
-            catch(Exception ex)
-            {
-                string message = $"{StringProvider.GetString("HouseholdQueryError")}{ex.Message}";
-                _showNotification(NotificationType.Error, message);
-            }
-            finally
-            {
-                IsLoading = false;
-            }
+            IsLoading = false;
         }
     }
 }
