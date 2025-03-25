be1059aba94b02d863d80dbf9bbc3e51f0cac42b
Batki Domonkos <domonkosbatki98@gmail.com>
2025-03-25 22:02:46 +0100

    Implemented map service

---
+++ ShoppingList.Persistor/Services/Interfaces/IMapService.cs
@@ -0,0 +1,12 @@
+﻿using ShoppingList.Core;
+
+namespace ShoppingList.Persistor.Services.Interfaces
+{
+    public interface IMapService
+    {
+        Task CreateMapAsync(int store_id, int x_size, int y_size, CancellationToken cancellationToken = default);
+        Task<Map> GetMapAsync(int store_id, CancellationToken cancellationToken = default);
+        Task UpdateMapAsync(int store_id, int x_size, int y_size, CancellationToken cancellationToken = default);
+        Task DeleteMapAsync(int store_id, CancellationToken cancellationToken = default);
+    }
+}
---
+++ ShoppingList.Persistor/Services/MapService.cs
@@ -0,0 +1,45 @@
+﻿using ShoppingList.Core;
+using ShoppingList.Persistor.Services.Interfaces;
+using System.Net.Http.Json;
+
+namespace ShoppingList.Persistor.Services
+{
+    internal class MapService(HttpClient client) : APIService(client), IMapService
+    {
+        public async Task CreateMapAsync(int store_id, int x_size, int y_size, CancellationToken cancellationToken = default)
+        {
+            var payload = new { x_size, y_size };
+
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync($"stores/{store_id}/map", payload, cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+        }
+
+        public async Task DeleteMapAsync(int store_id, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await _httpClient.DeleteAsync($"stores/{store_id}/map", cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+        }
+
+        public async Task<Map> GetMapAsync(int store_id, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"stores/{store_id}/map", cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+
+            Map? location = await response.Content.ReadFromJsonAsync<Map>(cancellationToken);
+
+            return location ?? throw new NullReferenceException();
+        }
+
+        public async Task UpdateMapAsync(int store_id, int x_size, int y_size, CancellationToken cancellationToken = default)
+        {
+            var payload = new { x_size, y_size };
+
+            HttpResponseMessage response = await _httpClient.PutAsJsonAsync($"stores/{store_id}/map", payload, cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+        }
+    }
+}
