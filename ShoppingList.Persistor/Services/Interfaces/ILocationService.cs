9c443c67d6f3edce740d637c4ee127dc9d83d303
Batki Domonkos <domonkosbatki98@gmail.com>
2025-03-25 21:45:18 +0100

    Implementing location service

---
+++ ShoppingList.Persistor/Services/Interfaces/ILocationService.cs
@@ -0,0 +1,12 @@
+﻿using ShoppingList.Core;
+
+namespace ShoppingList.Persistor.Services.Interfaces
+{
+    public interface ILocationService
+    {
+        Task CreateLocationAsync(int store_id, string country, string zip_code, string city, string street, string detail, CancellationToken cancellationToken = default);
+        Task<Location> GetLocationAsync(int store_id, CancellationToken cancellationToken = default);
+        Task UpdateLocationAsync(int store_id, string country, string zip_code, string city, string street, string detail, CancellationToken cancellationToken = default);
+        Task DeleteLocationAsync(int store_id, CancellationToken cancellationToken = default);
+    }
+}
---
+++ ShoppingList.Persistor/Services/LocationService.cs
@@ -0,0 +1,45 @@
+﻿using ShoppingList.Core;
+using ShoppingList.Persistor.Services.Interfaces;
+using System.Net.Http.Json;
+
+namespace ShoppingList.Persistor.Services
+{
+    internal class LocationService(HttpClient client) : APIService(client), ILocationService
+    {
+        public async Task CreateLocationAsync(int store_id, string country, string zip_code, string city, string street, string detail, CancellationToken cancellationToken = default)
+        {
+            var payload = new { country, zip_code, city, street, detail };
+
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync($"stores/{store_id}/location", payload, cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+        }
+
+        public async Task DeleteLocationAsync(int store_id, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await _httpClient.DeleteAsync($"stores/{store_id}/location", cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+        }
+
+        public async Task<Location> GetLocationAsync(int store_id, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"stores/{store_id}/location", cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+
+            Location? location = await response.Content.ReadFromJsonAsync<Location>(cancellationToken);
+
+            return location ?? throw new NullReferenceException();
+        }
+
+        public async Task UpdateLocationAsync(int store_id, string country, string zip_code, string city, string street, string detail, CancellationToken cancellationToken = default)
+        {
+            var payload = new { country, zip_code, city, street, detail };
+
+            HttpResponseMessage response = await _httpClient.PutAsJsonAsync($"stores/{store_id}/location", payload, cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+        }
+    }
+}
