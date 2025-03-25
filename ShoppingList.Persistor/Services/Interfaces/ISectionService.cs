452f6e687d15d613905492688cefb0c3454dd5eb
Batki Domonkos <domonkosbatki98@gmail.com>
2025-03-25 22:36:51 +0100

    Implementing section service

---
+++ ShoppingList.Persistor/Services/Interfaces/ISectionService.cs
@@ -0,0 +1,12 @@
+﻿using ShoppingList.Core;
+
+namespace ShoppingList.Persistor.Services.Interfaces
+{
+    public interface ISectionService
+    {
+        Task CreateSectionAsync(int map_id, string name, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Section>> GetSectionAsync(int map_id, CancellationToken cancellationToken = default);
+        Task UpdateSectionAsync(int map_id, int section_id, string name, CancellationToken cancellationToken = default);
+        Task DeleteSectionAsync(int map_id, int section_id, CancellationToken cancellationToken = default);
+    }
+}
---
+++ ShoppingList.Persistor/Services/SectionService.cs
@@ -0,0 +1,45 @@
+﻿using ShoppingList.Core;
+using ShoppingList.Persistor.Services.Interfaces;
+using System.Net.Http.Json;
+
+namespace ShoppingList.Persistor.Services
+{
+    internal class SectionService(HttpClient client) : APIService(client), ISectionService
+    {
+        public async Task CreateSectionAsync(int map_id, string name, CancellationToken cancellationToken = default)
+        {
+            var payload = new { name };
+
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync($"maps/{map_id}/sections", payload, cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+        }
+
+        public async Task DeleteSectionAsync(int map_id, int section_id, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await _httpClient.DeleteAsync($"maps/{map_id}/sections/{section_id}", cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+        }
+
+        public async Task<IEnumerable<Section>> GetSectionAsync(int map_id, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"maps/{map_id}/sections", cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+
+            IEnumerable<Section>? sections = await response.Content.ReadFromJsonAsync<IEnumerable<Section>>(cancellationToken);
+
+            return sections ?? throw new NullReferenceException();
+        }
+
+        public async Task UpdateSectionAsync(int map_id, int section_id, string name, CancellationToken cancellationToken = default)
+        {
+            var payload = new { name };
+
+            HttpResponseMessage response = await _httpClient.PutAsJsonAsync($"maps/{map_id}/sections/{section_id}", payload, cancellationToken);
+
+            await ValidateResponse(response, cancellationToken);
+        }
+    }
+}
